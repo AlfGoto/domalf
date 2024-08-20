965cfa6c2eb7a4c3261a38a11a01764f3ba6f1b1
AlfGoto <147511905+AlfGoto@users.noreply.github.com>
2024-08-20 16:09:05 +0200

    added: creAppend

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-export default function domAlf(string, parent = document.body) {
+export function domAlf(string, parent = document.body) {
     if (string.split('')[string.split('').length - 1] === '>' && string.split('')[0]) {
         parent.innerHTML += string
         let result = {}
-        Array.from(parent.querySelectorAll('[id]')).forEach(e=>{
+        Array.from(parent.querySelectorAll('[id]')).forEach(e => {
             result[e.id] = e
         })
         return result
@@ -10,3 +10,4 @@ export default function domAlf(string, parent = document.body) {
         console.error('argument is not a valid html tag: ', string)
     }
 }
+export function creAppend(pere, child, content = null) { let c = document.createElement(child); pere.appendChild(c); if (content) c.innerHTML = content; return c }
